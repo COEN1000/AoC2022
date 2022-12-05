cce792ca17a969d1fa7d771f884c29d7322b6554
Coen <cbunk@bouwatch.nl>
2022-12-05 16:44:34 +0100

    day3 part 1

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os,sys
-
+from difflib import SequenceMatcher
+import difflib
 
 def day1(filename: str):
     elves = []
@@ -16,13 +17,13 @@ def day1(filename: str):
                 calories = 0
             else:
                 calories += int(line)     
-    print(np.max(elves)) #pt1
+    print("D1 Part1 ",np.max(elves)) #pt1
     elves.sort(reverse=True)
 
     top3 = 0 
     for x in range(3):
         top3+=elves[x]
-    print(top3) #pt2
+    print("D1 Part2 ",top3) #pt2
 
 def day2(filename: str):
     PT2_STRAT = dict(A = dict(X = 4, Y = 8, Z = 3),B = dict(X = 1, Y = 5, Z = 9),C = dict(X = 7, Y = 2, Z = 6))
@@ -36,15 +37,27 @@ def day2(filename: str):
         for line in lines:
             pt1_score += PT1_STRAT[line[0]][line[2]]
             pt2_score += PT2_STRAT[line[0]][line[2]]
-        print(pt1_score, pt2_score)
+        print("D2 Part1: ",pt1_score, " D2 Part2: ",pt2_score)
 
 def day3(filename: str):
-    
+    sum = 0
     with open(filename, "r") as f:        
-        rucksack = f.readline
+        for line in f:
+            mid = len(line)
+            str1 = line[0:mid//2]
+            str2 = line[mid//2:]
+            
+            matcher = difflib.SequenceMatcher(a=str1, b=str2)
+            match = matcher.find_longest_match(0, len(matcher.a), 0, len(matcher.b))
 
+            if str1[match.a].isupper() :
+                sum+=ord(str1[match.a])-38
+            else:
+                sum+=ord(str1[match.a])-96
+
+    print("D3 part1:", sum)
 
 if __name__ == '__main__':
-    day1("input/day1.txt")
-    day2("input/day2.txt")
+    #day1("input/day1.txt")
+    #day2("input/day2.txt")
     day3("input/day3.txt")
